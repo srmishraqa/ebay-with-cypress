c748c6f240bfd7319e039af71f425446834cfaa8
Soumya Mishra <soumya_rm@outlook.com>
2023-05-26 13:50:16 +0530

    added 2nd test

--- cypress/e2e/ebayTests.spec.js
+++ cypress/e2e/ebayTests.spec.js
@@ -7,15 +7,20 @@ describe('ebay UI Tests', () => {
         })
     })
 
-    it('Access a Product via category after applying multiple filters', () => {
+    it('Scenario - 1 , Access a Product via category after applying multiple filters', () => {
+        //Navigate to Search by category > Electronics > Cell Phones & accessories
         cy.get('#gh-shop-a').click()
         cy.xpath("//a[text()='Cell phones & accessories']").click()
         cy.xpath("//span[text()='Cell Phones & Accessories']//parent::a").should('exist')
         cy.xpath("//span[text()='Cell Phones & Accessories']//parent::a").should('be.visible')
+
+        //After the page loads, click Cell Phones & Smartphones in the left hand side navigation section
         cy.xpath("//a[text()='Cell Phones & Smartphones']").should('be.visible')
         cy.xpath("//a[text()='Cell Phones & Smartphones']").click()
         cy.xpath("//span[text()='Cell Phones & Smartphones']//parent::a").should('exist')
         cy.xpath("//span[text()='Cell Phones & Smartphones']//parent::a").should('be.visible')
+
+        //Now, click – See All (appears under “Shop by brand” or “Shop by Network”)
         cy.xpath("(//span[text()='See All']//parent::button)[1]").click()
 
         //Apply screen size filter
@@ -42,7 +47,7 @@ describe('ebay UI Tests', () => {
         cy.xpath("//*[@id='c3-tray']//span[text()='$400 - $700']").should('be.visible')
         cy.xpath("//button[@aria-label='Apply']").click()
 
-        // Header Assertions
+        // Assertions for Heading which appears applying all the filter criteria
         cy.xpath('//h1//span')
             .invoke('text')
             .then((text) => {
@@ -57,5 +62,28 @@ describe('ebay UI Tests', () => {
         cy.xpath("//span[text()='Screen Size: 6 in or More']//parent::a").should('exist')
         cy.xpath("//span[text()='Price: $400.00 to $700.00']//parent::a").should('exist')
         cy.xpath("//span[text()='Item Location: US Only']//parent::a").should('exist')
+    })
+
+    it('Scenario - 2 , Access a Product via Search', () => {
+        //Type any search string in the search bar. For example: MacBook.
+        cy.get('#gh-ac').type('Macbook')
+        cy.wait(2500)
+        cy.xpath("//input[@value='Search']").click()
+        cy.wait(2500)
+        cy.xpath("//span[@class='BOLD' and text()='Macbook']").should('be.visible')
+
+        //Change the Search category. For example: Computers/Tablets & Networking and click Search.
+        cy.xpath("//span[text()='Computers/Tablets & Networking']//parent::a").click()
+        cy.wait(1000)
+
+        //Verify that the page loads completely.
+        cy.xpath("//span[@class='BOLD' and text()='Macbook']").should('be.visible')
+
+        //Verify that the first result name matches with the search string.
+        cy.xpath("(//li[starts-with(@id,'item')]//div[contains(@class,'item__title')]/span)[1]")
+            .invoke('text')
+            .then((text) => {
+                expect(text.replace(/\u00a0/g, ' ')).contain('Macbook')
+            })
     })
 })
